d0c1845489a73b0eb54c8fc1477d87ccbadd8ac9
Chris Sano <cmsano@gmail.com>
2017-03-02 17:34:07 -0800

    Removed redundant this prefix

--- Tennis/TennisTests.cs
+++ Tennis/TennisTests.cs
@@ -72,15 +72,15 @@ namespace Tennis
 
         private void CheckAllScores(ITennisGame game)
         {
-            var highestScore = Math.Max(this.player1Score, this.player2Score);
+            var highestScore = Math.Max(player1Score, player2Score);
             for (var i = 0; i < highestScore; i++)
             {
-                if (i < this.player1Score)
+                if (i < player1Score)
                     game.WonPoint("player1");
-                if (i < this.player2Score)
+                if (i < player2Score)
                     game.WonPoint("player2");
             }
-            Assert.AreEqual(this.expectedScore, game.GetScore());
+            Assert.AreEqual(expectedScore, game.GetScore());
         }
 
     }
