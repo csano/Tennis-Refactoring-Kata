85d3eb0ae2700dd37fc253a1909e9fcf608aa7fa
Chris Sano <cmsano@gmail.com>
2017-03-07 00:09:11 -0800

    Introduced PlayerScore class

--- Tennis/Scoreboard.cs
+++ Tennis/Scoreboard.cs
@@ -4,15 +4,25 @@ using System.Linq;
 
 namespace Tennis
 {
-    internal class Scoreboard
+    internal class PlayerScore
     {
         private readonly Player player1;
+
+        public PlayerScore(Player player1)
+        {
+            this.player1 = player1;
+        }
+    }
+
+    internal class Scoreboard
+    {
         private readonly Player player2;
         private readonly Dictionary<Player, Scoring> scores = new Dictionary<Player, Scoring>();
+        private readonly PlayerScore playerScore;
 
         public Scoreboard(Player player1, Player player2)
         {
-            this.player1 = player1;
+            playerScore = new PlayerScore(player1);
             this.player2 = player2;
             scores.Add(player1, 0);
             scores.Add(player2, 0);
@@ -27,16 +37,16 @@ namespace Tennis
         {
             if (scores.All(x => x.Value == scores.First().Value))
             {
-                return scores[player1] >= Scoring.Forty ? "Deuce" : $"{scores[player1]}-All";
+                return scores[playerScore.player1] >= Scoring.Forty ? "Deuce" : $"{scores[playerScore.player1]}-All";
             }
 
             if (scores.All(x => x.Value <= Scoring.Forty))
             {
-                return $"{scores[player1]}-{scores[player2]}";
+                return $"{scores[playerScore.player1]}-{scores[player2]}";
             }
 
             var leader = scores.OrderByDescending(x => x.Value).First().Key;
-            return Math.Abs(scores[player1] - scores[player2]) == 1 ? $"Advantage {leader.Name}" : $"Win for {leader.Name}";
+            return Math.Abs(scores[playerScore.player1] - scores[player2]) == 1 ? $"Advantage {leader.Name}" : $"Win for {leader.Name}";
         }
 
         private enum Scoring
