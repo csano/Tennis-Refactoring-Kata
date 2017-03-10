0751424a1599af9c39daab1408a170be2d5ddb3a
Chris Sano <cmsano@gmail.com>
2017-03-10 09:45:03 -0800

    Updated mapping to be a list instead of a dictionary

--- Tennis/RuleStringConverter.cs
+++ Tennis/RuleStringConverter.cs
@@ -6,20 +6,20 @@ namespace Tennis
 {
     internal class RuleStringConverter
     {
-        private static Dictionary<Type, IConditionStringConverter> GetStringConverters()
+        private static IEnumerable<IConditionStringConverter> GetStringConverters()
         {
-            return new Dictionary<Type, IConditionStringConverter>
+            return new List<IConditionStringConverter>
             {
-                { typeof(TieRule), new TieConditionStringConverter() },
-                { typeof(ScoreIsNotATieRule), new ScoreIsNotATieStringConverter() },
-                { typeof(AdvantageRule), new AdvantageRuleStringConverter() },
-                { typeof(WinnerCondition), new WinnerStringConverter() }
+                 new TieConditionStringConverter() ,
+                 new ScoreIsNotATieStringConverter() ,
+                 new AdvantageRuleStringConverter() ,
+                 new WinnerStringConverter()
             };
         }
 
         public string Convert(IScoringCondition scoringCondition, PlayerScore player1Score, PlayerScore player2Score) 
         {
-            return GetStringConverters().Where(x => x.Key == scoringCondition.GetType()).Select(x => x.Value).FirstOrDefault()?.Convert(player1Score, player2Score);
+            return GetStringConverters().FirstOrDefault(x => x.ConditionType == scoringCondition.GetType())?.Convert(player1Score, player2Score);
         }
     }
 }
