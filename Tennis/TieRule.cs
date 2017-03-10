5fd3cab03ac34ec71d87ccb567a10fecf8421e8e
Chris Sano <cmsano@gmail.com>
2017-03-10 09:46:58 -0800

    Removed commented out code

--- Tennis/TieRule.cs
+++ Tennis/TieRule.cs
@@ -5,7 +5,6 @@ namespace Tennis
         public override bool Evaluate(PlayerScore player1Score, PlayerScore player2Score)
         {
             return PlayerScoresAreEqual(player1Score, player2Score);
-                //return player1Score.Score >= Scoring.Forty ? "Deuce" : $"{player1Score.Score}-All";
         }
     }
 }
